559a0eb68a962f28ac136e70fcdb76a5e861ecac
Vivian McNaughton <vmcnaughton@illinoislegalaid.org>
2026-05-18 13:59:57 -0500

    Potential fix for addendum template bug

--- setup.py
+++ setup.py
@@ -44,17 +44,16 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.CivilNoContactOrderRequest',
-      version='1.0.4',
+      version='1.0.5',
       description=('A docassemble extension.'),
       long_description='# docassemble.CivilNoContactOrderRequest\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nVivian McNaughton, vmcnaughton@illinoislegalaid.org\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Vivian McNaughton',
       author_email='vmcnaughton@illinoislegalaid.org',
-      license='The MIT License (MIT)',
+      license='MIT',
       url='https://docassemble.org',
       packages=find_namespace_packages(),
       install_requires=[],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/CivilNoContactOrderRequest/', package='docassemble.CivilNoContactOrderRequest'),
      )
-
